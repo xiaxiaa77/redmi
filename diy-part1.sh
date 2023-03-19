ea905e5cdc806527d382ba14b1a41beeebbdaa53
xiaxiaa77 <126748286+xiaxiaa77@users.noreply.github.com>
2023-03-20 04:35:41 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,7 +21,7 @@ make menuconfig
 
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 
 # Add a feed source
