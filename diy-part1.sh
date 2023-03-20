c299ed828f679b77cb2e7f290ce376c002f3d2ec
xiaxiaa77 <126748286+xiaxiaa77@users.noreply.github.com>
2023-03-20 15:55:49 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,9 +14,21 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
+# 1.清除缓存
+rm -rf tmp
+
+# 2.feeds.conf文件添加源
+src-git helloworld https://github.com/fw876/helloworld
+src-git passwall https://github.com/xiaorouji/openwrt-passwall
+
+# 3.重新执行升级安装下载编译等操作
+./scripts/feeds update -a
+./scripts/feeds install -a
+make -j8 download V=s
+make -j1 V=s
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
